b8945bd1aa6da5d2f388fb014cf8d87f30a5085d
MrInformatic <maxundmoritz99199@gmail.com>
2018-03-21 21:44:39 +0100

    Improved VulkanType

--- src/vulkan/wrapper/registry/vtype/VulkanType.scala
+++ src/vulkan/wrapper/registry/vtype/VulkanType.scala
@@ -6,8 +6,8 @@ import scala.xml.Node
 
 abstract class VulkanType(registry: Registry,val node: Node) extends RegistryType(registry){
   val name: String = node \\@ "name"
-  val requires: Option[String] = node \@@ "requires"
-  val alias: Option[String] = node \@@ "alias"
+  val requires: Option[VulkanType] = (node \@@ "requires").flatMap(registry.types.get)
+  val alias: Option[VulkanType] = (node \@@ "alias").flatMap(registry.types.get)
   val api: Option[String] = node \@@ "api"
   val comment: Option[String] = node \@@ "comment"
 }
